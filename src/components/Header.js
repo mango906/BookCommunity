b77b087d4f214fc4babd24ad5dc5941836d4a41a
민경빈 <minkb01@gmail.com>
2019-11-24 19:14:39 +0900

    [FIX] 헤더에 작성하기 버튼 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,59 @@
 import React from "react";
 import styled from "styled-components";
+import { FaArrowLeft } from "react-icons/fa";
+import { IoMdArrowBack } from "react-icons/io";
 
 const WriteHeader = styled("div")`
    background-color: #343a40;
    height: 4rem;
    display: flex;
    align-items: center;
-   padding-left: 2rem;
+   padding: 0px 1rem;
 `;
 
 const TitleInput = styled("input")`
    background: none;
-   font-size: 1.25rem;
+   font-size: 1.2rem;
    color: #fff;
    outline: none;
    border: none;
+   margin-left: 1rem;
+`;
+
+const Button = styled("button")`
+   background-color: #a61e4d;
+   color: white;
+   border: none;
+   outline: none;
+   border-radius: 2px;
+   padding: 5px 15px;
+   cursor: pointer;
+   font-size: 0.875rem;
+`;
+
+const Actions = styled("div")`
+   display: flex;
+   margin-left: auto;
+`;
+
+const BackBtn = styled(IoMdArrowBack)`
+   color: #cccccc;
+   cursor: pointer;
+   font-size: 1.75rem;
+
+   &:hover {
+      color: #fff;
+   }
 `;
 
 const Header = ({ handleChange }) => {
    return (
       <WriteHeader>
-         <TitleInput
-            onChange={e => handleChange(e.target.value)}
-            placeholder="제목을 입력해주세요..."
-         ></TitleInput>
+         <BackBtn></BackBtn>
+         <TitleInput placeholder="제목을 입력해주세요..." />
+         <Actions>
+            <Button>작성하기</Button>
+         </Actions>
       </WriteHeader>
    );
 };
